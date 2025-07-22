c94a7a172a0235ee40a5c87f9cfb745823faab0d
Gustavo Norberto <gustavobarros12654@gmail.com>
2025-07-22 18:51:18 -0300

    New steps

---
+++ Features/Steps/stepDefinition.py
@@ -0,0 +1,23 @@
+@given(u'User is on Registrattion page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given User is on Registrattion page')
+
+
+@when(u'User enter username')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User enter username')   
+
+
+@when(u'User enter password')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User enter password')   
+
+
+@when(u'User click on signup button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User click on signup button')
+
+
+@then(u'User should be registered successfully')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then User should be registered successfully')
